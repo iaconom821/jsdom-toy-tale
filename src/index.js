7db2e3c22f9705e628a95eb9b19bfade24e97ecd
iaconom821 <iaconom821@gmail.com>
2021-03-24 11:39:40 -0400

    up to question 2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const toyCollection = document.querySelector("#toy-collection")
+  const getToyForm = document.querySelector('.add-toy-form')
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +15,61 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+  .then(function (json) {
+  json.forEach(function (obj) {
+    turnToysIntoHTMLElementsInCollection(obj)
+  })
+  })
+
+  function turnToysIntoHTMLElementsInCollection(jsonObj) {
+    const getToys = document.createElement('div');
+          getToys.classList.add('card')
+
+    const toySpan = document.createElement('span')
+    
+    const toyName = document.createElement('h2')
+          toyName.innerText = jsonObj.name
+    
+    const toyImage = document.createElement('img')
+          toyImage.src = jsonObj.image 
+          toyImage.classList.add('toy-avatar')
+    
+    const toyLikes = document.createElement('p')
+          toyLikes.innerText = jsonObj.likes 
+
+    const toyLikesButton = document.createElement('button')
+          toyLikesButton.classList.add('like-btn')
+
+    toySpan.append(toyName, toyImage, toyLikes, toyLikesButton)
+    getToys.append(toySpan)
+    toyCollection.append(getToys);
+  }
+
+  getToyForm.addEventListener('submit', function(evt) {
+    evt.preventDefault()
+    const newToyName = evt.target.name.value
+    const newToyImage = evt.target.image.value
+    
+    fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        "name": `${newToyName}`,
+        "image": `${newToyImage}`,
+        "likes": 0
+      })
+    })
+    .then(res => res.json())
+    .then(function (newObj){ 
+      turnToysIntoHTMLElementsInCollection(newObj)
+    })
+  })
 });
+
+
